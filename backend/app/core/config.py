d3037132f5c1d3309316d5709373458ba806ff80
Kamil Grundas <kamil.grundas@gmail.com>
2025-09-07 12:22:54 +0200

    Add initial backend implementation with user authentication and database setup

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,11 @@
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
 import secrets
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import (
+    PostgresDsn,
+    computed_field,
+)
 
 
 
@@ -12,8 +17,33 @@ class Settings(BaseSettings):
         extra="ignore",
     )
     API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     
     PROJECT_NAME: str
+
+    # Superuser
+    FIRST_SUPERUSER: str
+    FIRST_SUPERUSER_PASSWORD: str
+
+    # Postgres
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
 
 
 settings = Settings() # type: ignore
