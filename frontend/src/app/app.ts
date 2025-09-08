34b7e575b1824e8eb12f368dd83efcd0fa8e7225
Kamil Grundas <kamil.grundas@gmail.com>
2025-09-08 18:40:45 +0200

    Frontend: Add About, Navbar, and Overview sections with styling

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Overview } from './sections/overview/overview';
+import { Navbar } from './sections/navbar/navbar';
+import { About } from './sections/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Overview, Navbar, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ frontend/src/app/sections/overview/overview.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  imports: [],
+  templateUrl: './overview.html',
+  styleUrl: './overview.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Overview {
+
+}
