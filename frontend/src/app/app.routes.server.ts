63fbfb6259b0372c5c9e44f12134acf6e4e2eafa
Kamil Grundas <kamil.grundas@gmail.com>
2026-03-14 09:49:08 +0100

    Fix SSR routes after routing refactor

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,14 +2,8 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-<<<<<<< HEAD
-    path: 'portfolio/:user_url',
-    renderMode: RenderMode.Server,
-=======
     path: ':user_url',
-    renderMode: RenderMode.Prerender,
-    getPrerenderParams: () => Promise.resolve([{ user_url: 'john_doe' }]),
->>>>>>> d864fc8 (Frontend: Refactor routing paths to remove 'portfolio/' prefix and update title handling in login and portfolio components)
+    renderMode: RenderMode.Server,
   },
   {
     path: '**',
